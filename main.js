d56b748d4a1e8e0a34fb6f67dd540b3ac0d9e13f
IgnacioDN <ignaciodiazneila@gmail.com>
2022-05-10 11:12:06 -0300

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.newscards', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-section', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-section-one', { delay: 500 });
